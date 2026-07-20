07afb7cded5761a3a97d27f794ca18bd169a5a42
Mirco <mirkosair28@gmail.com>
2026-07-20 14:45:09 -0500

    fix: update redirect route in store method to point to dashboard

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -20,7 +20,7 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
         $request->session()->regenerate();
 
-        return redirect()->intended(route('internal', absolute: false));
+        return redirect()->intended(route('dashboard', absolute: false));
     }
 
     public function destroy(Request $request): RedirectResponse
