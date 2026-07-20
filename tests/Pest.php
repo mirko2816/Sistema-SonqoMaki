a4a930a8b8a7497824086cfc2a90356689e2a3a7
Mirco <mirkosair28@gmail.com>
2026-07-20 14:48:49 -0500

    feat: add specialist user creation function to Pest testing framework

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,16 @@
 <?php
 
+use App\Models\User;
 use Tests\TestCase;
 
 pest()->extend(TestCase::class)
     ->in('Feature');
+
+function specialist(array $attributes = []): User
+{
+    return User::create(array_merge([
+        'email' => 'especialista@sonqomaki.test',
+        'password' => 'Una-clave-segura-2026',
+        'is_active' => true,
+    ], $attributes));
+}
